d0c75c2df745a58787ba99ec2f6ec3f8792635c3
Charlie <chzheng.2@gmail.com>
2017-07-19 19:40:34 -0500

    Initial new-branch commit

--- app/src/main/java/com/afrikappakorps/sticksandstones/GameActivity.java
+++ app/src/main/java/com/afrikappakorps/sticksandstones/GameActivity.java
@@ -1,60 +1,56 @@
 package com.afrikappakorps.sticksandstones;
 
-import android.os.CountDownTimer;
-import android.os.PersistableBundle;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.widget.TextView;
+import android.widget.Chronometer;
 
 public class GameActivity extends AppCompatActivity {
-
-    private TextView mTimerView;
     public static final String IS_NEW_GAME = "isNewGame";
-
     private int match, round, player1, player2, judge;
-    public static final String MATCH_KEY = "match", ROUND_KEY = "round",
-        PLAYER1_KEY = "player1", PLAYER2_KEY = "player2", JUDGE_KEY = "judge";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        mTimerView = (TextView) findViewById(R.id.text_timer);
 
-        //Variable setup
+        //Key-Value setup
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
         if (getIntent().getExtras().getBoolean(IS_NEW_GAME)) {
-            //TODO: Save shit in the database
+            SharedPreferences.Editor prefEditor = sharedPref.edit();
+            prefEditor.clear();
+            prefEditor.apply(); //If no work, use .commit()
         }
+
+        match = sharedPref.getInt("match", 1);
+        round = sharedPref.getInt("round", 1);
+        player1 = sharedPref.getInt("player1", 0);
+        player2 = sharedPref.getInt("player2", 1);
+        judge = sharedPref.getInt("judge", 2);
 
         //App bar setup
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar_game));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Match " + match + ", Round " + round);
-
-        //Timer setup
-        new CountDownTimer(60000, 1000) {
-            @Override
-            public void onTick(long length) {
-                mTimerView.setText(String.valueOf(length / 1000));
-            }
-
-            @Override
-            public void onFinish() {
-                String done = "Done!";
-                mTimerView.setText(done);
-            }
-
-        }.start();
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt(MATCH_KEY, match);
-        outState.putInt(ROUND_KEY, round);
-        outState.putInt(PLAYER1_KEY, player1);
-        outState.putInt(PLAYER2_KEY, player2);
-        outState.putInt(JUDGE_KEY, judge);
+    protected void onResume() {
+        super.onResume();
+
+        //Start timer
+        Chronometer timer = (Chronometer) findViewById(R.id.timer);
+        timer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //Stop timer
+        Chronometer timer = (Chronometer) findViewById(R.id.timer);
+        timer.stop();
     }
 }
